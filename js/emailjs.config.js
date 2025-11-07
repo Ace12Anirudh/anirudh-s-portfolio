617d78cde9711cb5aaae0cf089156ec117e3a2ab
ACE <rakshak28anirudh@gmail.com>
2025-11-07 21:26:54 +0530

    updates

--- js/emailjs.config.js
+++ js/emailjs.config.js
@@ -6,6 +6,7 @@ window.EMAILJS_CONFIG = {
   PUBLIC_KEY: "oIP4dAamMMgllbUo7",
   SERVICE_ID: "service_7lfet8h",
   TEMPLATE_ID: "template_4sym7hw",
+  DOWNLOAD_ID: "template_293wvmn"
 };
 
 (function initEmailJS(){
